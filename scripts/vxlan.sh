79bd51b3b614e671b1e093ea8999cfc79a683e40
nowgood <851536944wb@gmail.com>
2017-06-02 20:08:57 +0800

    Create vxlan.sh

---
+++ scripts/vxlan.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e 
+ovs-vsctl add-br br0
+ovs-vsctl add-port br0 eth0
+ifconfig eth0 0 up && sudo ifconfig br0 192.168.64.136/24 up
+route add default gw 192.168.64.2 br0
+
+ovs-vsctl add-br br1
+ifconfig br1 10.0.1.1/24 up
+
+ovs-vsctl add-port br0 patch-to-br1 \
+        -- set interface patch-to-br1 type=patch option:peer=patch-to-br0 \
+        -- add-port br1 patch-to-br0 \
+        -- set interface patch-to-br0 type=patch option:peer=patch-to-br1
+
+sudo ovs-vsctl  add-port br1 vxlan -- set interface vxlan type=vxlan option:remote_ip=192.168.64.131 option:key=1234
